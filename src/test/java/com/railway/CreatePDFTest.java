27c9fd2d04563754071182b8cc0d57b2ac5d4118
Sani Jana <sanijana15@gmail.com>
2024-07-02 01:37:15 +0530

    Added sample template of the ticket pdf with data

---
+++ src/test/java/com/railway/CreatePDFTest.java
@@ -0,0 +1,51 @@
+package com.railway;
+
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.xmlgraphics.util.MimeConstants;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class CreatePDFTest {
+    public static void main(String[] args) {
+        try {
+            // Initialize FOP factory and configuration
+            FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
+            FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+
+            // Setup output stream
+            OutputStream out = new FileOutputStream(new File("data/output.pdf"));
+
+            // Construct FOP with desired output format
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+
+            // Setup XSLT
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer(new StreamSource(new File("src/test/resources/template.xsl")));
+
+            // Setup input for XSLT transformation
+            StreamSource src = new StreamSource(new File("src/test/resources/data.xml"));
+
+            // Resulting SAX events must be piped through to FOP
+            SAXResult res = new SAXResult(fop.getDefaultHandler());
+
+            // Start XSLT transformation and FOP processing
+            transformer.transform(src, res);
+
+            // Close output stream
+            out.close();
+
+            System.out.println("PDF Created Successfully!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
